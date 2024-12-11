eaa6dcac9c926c813195e2a320f12603495b2e64
Raz Danielyan <razmikdanielyan179@gmail.com>
2024-12-11 20:15:26 +0400

    commit

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -39,32 +39,32 @@ export const protectedRouts = {
   },
 };
 
-// const routes = {
-//   signIn: {
-//     pageTitle: 'Sign In',
-//     pathname: '/sign-in',
-//     key: 'signIn',
-//     component: 'pages/guest/sign-in.page',
-//   },
-//   signUp: {
-//     pathname: '/sign-up',
-//     key: 'signUp',
-//     component: 'pages/guest/sign-up.page',
-//     pageTitle: 'Sign Up',
-//   },
-//   forgotPass: {
-//     pathname: '/forgot-password',
-//     key: 'forgotPass',
-//     component: 'pages/guest/forgot-password.page',
-//     pageTitle: 'Forgot Password',
-//   },
-//   resetPass: {
-//     pathname: '/reset-password',
-//     key: 'resetPass',
-//     component: 'pages/guest/reset-password.page',
-//     pageTitle: 'Reset Password',
-//   },
-// };
+const routes = {
+  signIn: {
+    pageTitle: 'Sign In',
+    pathname: '/sign-in',
+    key: 'signIn',
+    component: 'pages/guest/sign-in.page',
+  },
+  signUp: {
+    pathname: '/sign-up',
+    key: 'signUp',
+    component: 'pages/guest/sign-up.page',
+    pageTitle: 'Sign Up',
+  },
+  forgotPass: {
+    pathname: '/forgot-password',
+    key: 'forgotPass',
+    component: 'pages/guest/forgot-password.page',
+    pageTitle: 'Forgot Password',
+  },
+  resetPass: {
+    pathname: '/reset-password',
+    key: 'resetPass',
+    component: 'pages/guest/reset-password.page',
+    pageTitle: 'Reset Password',
+  },
+};
 
 export const roleDefPaths = {
   customer: '/',
@@ -72,4 +72,4 @@ export const roleDefPaths = {
   admin: '/',
 };
 
-// export default routes;
+export default routes;
